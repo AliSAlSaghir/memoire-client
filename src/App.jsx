b8f10dc2c342f101d615bfaaa4b888fd473b1d29
Ali AlSaghir <alisalsaghir@gmail.com>
2025-07-23 10:05:28 +0300

    add session handler and toast notifications; update private routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,20 @@ import Explore from "./pages/public/Explore";
 import About from "./pages/public/About";
 import Contact from "./pages/public/Contact";
 
-import Home from "./pages/private/Home";
 import CreateCapsule from "./pages/private/CreateCapsule";
 import Profile from "./pages/private/Profile";
 import CapsuleDetails from "./pages/private/CapsuleDetails";
 import CapsuleWall from "./pages/private/CapsuleWall";
+import MyCapsules from "./pages/private/MyCapsules";
+import { ToastContainer } from "react-toastify";
+import UnlistedCapsule from "./pages/private/UnlistedCapsule";
+import SessionHandler from "./components/SessionHandler";
 
 function App() {
   return (
     <Router>
+      <SessionHandler />
+      <ToastContainer />
       <Routes>
         {/* Public Routes */}
         <Route element={<PublicLayout />}>
@@ -34,10 +39,12 @@ function App() {
             </ProtectedRoute>
           }
         >
-          <Route path="/home" element={<Home />} />
-          <Route path="/create" element={<CreateCapsule />} />
           <Route path="/capsules" element={<CapsuleWall />} />
+          <Route path="/create" element={<CreateCapsule />} />
+          <Route path="/my-capsules" element={<MyCapsules />} />
           <Route path="/capsules/:capsuleId" element={<CapsuleDetails />} />
+          <Route path="/unlisted_capsule/:token" element={<CapsuleDetails />} />
+          <Route path="/unlisted" element={<UnlistedCapsule />} />
           <Route path="/profile" element={<Profile />} />
         </Route>
       </Routes>
