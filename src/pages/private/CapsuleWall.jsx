68cce9a71823d50aeee5a4b6747ba77d95307632
Ali AlSaghir <alisalsaghir@gmail.com>
2025-07-23 10:11:10 +0300

    refactor: remove unused private page components

--- src/pages/private/CapsuleWall.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CapsuleWall = () => {
-  return <div>CapsuleWall</div>;
-};
-
-export default CapsuleWall;
--- src/pages/private/CreateCapsule.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CreateCapsule = () => {
-  return <div>CreateCapsule</div>;
-};
-
-export default CreateCapsule;
